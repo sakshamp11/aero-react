b99c7e1aac9bf3b7d2465ec5e84b04332150ee13
sakshamp11 <rimpi.saksham@gmail.com>
2022-07-27 11:00:31 +0530

    first commit

---
+++ webpages/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+function App() {
+  return (
+    <div style={{backgroundColor:"whitesmoke",border:"1px solid black"}}>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ webpages/src/components/Navbar.js
@@ -0,0 +1,150 @@
+import React, { Component, useState } from 'react'
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+
+import TextField from '@mui/material/TextField';
+import "./Navbar.css"
+let logincardFlag = true;
+// function logincard(props){
+//     return{
+
+//     }
+
+
+// }
+const renderErrorMessage = (name) => {
+
+}
+
+
+const renderForm = () => {
+    return (
+        <div className="form" >
+            <form className='formWrapper'>
+                <div className="input-container">
+                    <div className='wrapperLogin'>
+                        <Box className='Login-inp'
+
+                        >
+                            <div className='inpar'><TextField fullWidth label="Mobile No. or Email" id="Mobile No. or Email" /></div>
+                            <div className='inpar'><TextField fullWidth label="Mobile No. or Email" id="Mobile No. or Email" /></div>
+
+                            <br></br>
+
+                        </Box>
+
+
+
+
+                    </div>
+                </div>
+                <div className='middle'>
+                <Button  style={{marginLeft:"35%",marginBottom:"3%",height:"100%"}} variant="contained" size="large">
+                    Login
+                </Button>
+                <div>forgot password?</div>
+                </div>
+                <div className='creatacc'>
+                   <div style={{marginLeft:"35%"}}>
+                   Create account 
+                   </div>
+                </div>
+
+            </form>
+        </div>
+    )
+}
+export class Navbar extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+
+            errorMessages: [],
+            isSubmitted: false
+        };
+    }
+    render() {
+        return (
+            <>
+            <div className="whole-wrapper">
+
+                <div class="header">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-4 company-logo">
+                                <img src="images/logo.png"></img>
+
+                            </div>
+                            <div class="col-lg-8 menu">
+                                <nav class="navbar navbar-expand-lg navbar-light ">
+
+                                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                                        aria-expanded="false" aria-label="Toggle navigation">
+                                        <span class="navbar-toggler-icon"></span>
+                                    </button>
+
+                                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                        <div class="nav-list">
+
+
+                                            <h4 class="nav-link" >Home</h4>
+
+
+
+                                            <h4 class="nav-link" href="#">About us</h4>
+
+
+
+                                            <h4 class="nav-link" href="#">Services</h4>
+
+
+                                            <h4 class="nav-link" href="#">Why Us</h4>
+
+
+
+                                            <h4 class="nav-link" href="#">Pricing</h4>
+
+
+                                            <h4 class="nav-link" href="#">Contact</h4>
+
+
+                                            <div className='LoginNav' >
+                                                <Button variant="contained" size="small">
+                                                    Login
+                                                </Button>
+                                            </div>
+
+
+                                        </div>
+                                    </div>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='bodyPage'>
+
+                    <div className='info-org'>
+                        <h6>SEARCH MY 
+                        <br></br>
+                        TENDER PLEASE</h6>
+
+                    </div>
+
+                    <div className='card-con'>
+                        {(logincardFlag) ? renderForm() : <div ></div>}
+                    </div>
+
+                </div>
+
+
+            </div>
+            <div style={{backgroundColor:"whitesmoke"}}></div>
+            </>
+        )
+    }
+}
+
+export default Navbar
