c290c69590d4d99be4bb5ff27024f5aa5c57a9e7
sakshamp11 <rimpi.saksham@gmail.com>
2022-08-02 17:01:10 +0530

    commit

--- webpages/src/App.js
+++ webpages/src/App.js
@@ -2,14 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Register  from './components/Register'
+import Price from './components/Price';
+import Services from './components/Services';
+import SearchList from './components/SearchList';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     // <div style={{backgroundColor:"whitesmoke",border:"1px solid black"}}>
-    //   <Navbar/>
+      <Navbar/>
     // </div>
 
-      <Register/>
+      // <Register/>
+      // <Price/>
+
   );
 }
 
+
 export default App;
+ 
--- webpages/src/components/Navbar.js
+++ webpages/src/components/Navbar.js
@@ -2,7 +2,7 @@ import React, { Component, useState } from 'react'
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import CloseButton from 'react-bootstrap/CloseButton';
-
+import axios from 'axios';
 import TextField from '@mui/material/TextField';
 import "./Navbar.css"
 
@@ -40,7 +40,7 @@ const renderForm = () => {
 
                         </Box>
                         <br></br>
-                    
+
                     </div>
                 </div>
                 <div>forgot password?</div>
@@ -48,14 +48,14 @@ const renderForm = () => {
                     <Button style={{ marginLeft: "35%", marginBottom: "3%", height: "100%" }} variant="contained" size="large">
                         Login
                     </Button>
-                    
+
                 </div>
                 <div className='creatacc'>
                     <div style={{ marginLeft: "35%" }}>
                         Create an  account
-                        <Button variant="outlined" style={{marginTop:"0.5vh",marginBottom:"0,5vh"}}>Register as Business</Button>
+                        <Button variant="outlined" style={{ marginTop: "0.5vh", marginBottom: "0,5vh" }}>Register as Business</Button>
                         <br></br>
-                        <Button variant="contained" style={{marginTop:"0.5vh",marginBottom:"0,5vh"}}>Register as User</Button>
+                        <Button variant="contained" style={{ marginTop: "0.5vh", marginBottom: "0,5vh" }}>Register as User</Button>
                     </div>
                 </div>
 
@@ -63,106 +63,207 @@ const renderForm = () => {
         </div>
     )
 }
-export class Navbar extends Component {
+function Navbar() {
 
-    constructor() {
-        super();
-        this.state = {
+    // constructor() {
+    //     super();
+    //     this.state = {
 
-            errorMessages: [],
-            isSubmitted: false
-        };
-    }
-    render() {
-        return (
-            <>
-                <div className="whole-wrapper">
+    //         errorMessages: [],
+    //         isSubmitted: false,
 
-                    <div class="header">
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-lg-4 company-logo">
-                                    <img src="images/logo.png"></img>
+    //         name:'',
+    //         email:'',
+    //         mobile:''
+
+    //     };
+    //}
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+
+
+    return (
+        <>
+            <div className="whole-wrapper">
+
+                <div class="container">
+                    <div className='Navb'>
+                        <div>
+
+                        </div>
+                        <nav class="navbar navbar-expand-lg navbar-light">
+
+                            <button class="navbar-toggler" type="button" data-toggle="collapse"
+                                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                                aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+
+                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <div class="nav-list">
+
+
+                                    <h4 class="nav-link" >Home</h4>
+
+
+
+                                    <h4 class="nav-link" href="#">About us</h4>
+
+
+
+                                    <h4 class="nav-link" href="#">Services</h4>
+
+
+                                    <h4 class="nav-link" href="#">WhyUs</h4>
+
+
+
+                                    <h4 class="nav-link" href="#">Pricing</h4>
+
+
+                                    <h4 class="nav-link" href="#">Contact</h4>
+
+
+
+                                    <Button variant="contained" size="small" style={{ marginTop: "1.2vh", height: "70%", marginRight: "0.7vw" }}>
+                                        Login
+                                    </Button>
+
+
 
                                 </div>
-                                <div class="col-lg-8 menu">
-                                    <nav class="navbar navbar-expand-lg navbar-light ">
-
-                                        <button class="navbar-toggler" type="button" data-toggle="collapse"
-                                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                                            aria-expanded="false" aria-label="Toggle navigation">
-                                            <span class="navbar-toggler-icon"></span>
-                                        </button>
-
-                                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                                            <div class="nav-list">
-
-
-                                                <h4 class="nav-link" >Home</h4>
-
-
-
-                                                <h4 class="nav-link" href="#">About us</h4>
-
-
-
-                                                <h4 class="nav-link" href="#">Services</h4>
-
-
-                                                <h4 class="nav-link" href="#">Why Us</h4>
-
-
-
-                                                <h4 class="nav-link" href="#">Pricing</h4>
-
-
-                                                <h4 class="nav-link" href="#">Contact</h4>
-
-
-                                                <div className='LoginNav' >
-                                                    <Button variant="contained" size="small">
-                                                        Login
-                                                    </Button>
-                                                </div>
-
-
-                                            </div>
-                                        </div>
-                                    </nav>
-                                </div>
                             </div>
-                        </div>
+                        </nav>
                     </div>
-                    <div className='bodyPage'>
-
-                        <div className='org'>
-                            <div className='info-org-slogan'>
-                                SEARCH MY
-                                <br></br>
-                                TENDER PLEASE
-                            </div>
-                            <br></br>
-                            <div className='info-org-desc'>
-                                Your one stop solution for a seamless,optimized
-                                <br></br>
-                                and automated tender searching experience
-                            </div>
-
-                        </div>
-
-                        <div className='card-con'>
-                            {(logincardFlag) ? renderForm() : <div ></div>}
-                        </div>
-
-                    </div>
-
 
                 </div>
-                <div style={{ backgroundColor: "whitesmoke" }}></div>
-            </>
-        )
-    }
+                <div className='bodyPage'>
+
+                    <div className='org'>
+                        <div className='info-org-slogan'>
+                            SEARCH MY
+                            <br></br>
+                            TENDER PLEASE
+                        </div>
+                        <br></br>
+                        <div className='info-org-desc'>
+                            Your one stop solution for a seamless,optimized
+                            <br></br>
+                            and automated tender searching experience
+                        </div>
+
+                    </div>
+
+                    <div className='card-con'>
+                        {(logincardFlag) ? <div className="form" >
+                            <CloseButton></CloseButton>
+                            <form className='formWrapper'>
+                                <div className="input-container">
+                                    <div className='wrapperLogin'>
+
+                                        <Box className='Login-inp'
+
+                                        >   <div style={{
+                                            fontStyle: "font-family: 'Helvetica', 'Arial', sans-serif", textDecoration: "underline",
+                                            textDecorationColor: "blue"
+                                        }}><b>Login to your account</b></div>
+                                            <div className='inpar'><TextField fullWidth label="Mobile No. or Email" id="Mobile No. or Email" onChange={(e) => setEmail(e.target.value)} /></div>
+                                            <div className='inpar'><TextField fullWidth label="Password" id="Password" onChange={(e) => setPassword(e.target.value)}/></div>
+
+                                            <br></br>
+
+                                        </Box>
+                                        <br></br>
+                                        
+
+                                        
+                                    </div>
+                                </div>
+                                <div>forgot password?</div>
+                                <div className='middle'>
+                                    <Button style={{ marginLeft: "35%", marginBottom: "3%", height: "100%" }} variant="contained" size="large" onClick={()=>{
+                                        console.log({email});
+                                        console.log({password});
+                                    }}>
+                                        Login
+                                    </Button>
+
+                                </div>
+                                <div className='creatacc'>
+                                    <div style={{ marginLeft: "35%" }}>
+                                        Create an  account
+                                        <Button variant="outlined" style={{ marginTop: "0.5vh", marginBottom: "0,5vh" }}>Register as Business</Button>
+                                        <br></br>
+                                        <Button variant="contained" style={{ marginTop: "0.5vh", marginBottom: "0,5vh" }}>Register as User</Button>
+                                    </div>
+                                </div>
+
+                            </form>
+                        </div> : <div ></div>}
+                    </div>
+
+                </div>
+
+
+            </div>
+            <div style={{ backgroundColor: "whitesmoke" }}></div>
+            <footer class="footer-bs">
+                <div class="row">
+                    <div class="col-md-3 footer-brand animated fadeInLeft">
+                        <h2>Logo</h2>
+                        <p>Suspendisse hendrerit tellus laoreet luctus pharetra. Aliquam porttitor vitae orci nec ultricies. Curabitur vehicula, libero eget faucibus faucibus, purus erat eleifend enim, porta pellentesque ex mi ut sem.</p>
+                        <p>© 2014 BS3 UI Kit, All rights reserved</p>
+                    </div>
+                    <div class="col-md-4 footer-nav animated fadeInUp">
+                        <h4>Menu —</h4>
+                        <div class="col-md-6">
+                            <ul class="pages">
+                                <li><a href="#">Travel</a></li>
+                                <li><a href="#">Nature</a></li>
+                                <li><a href="#">Explores</a></li>
+                                <li><a href="#">Science</a></li>
+                                <li><a href="#">Advice</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-md-6">
+                            <ul class="list">
+                                <li><a href="#">About Us</a></li>
+                                <li><a href="#">Contacts</a></li>
+                                <li><a href="#">Terms & Condition</a></li>
+                                <li><a href="#">Privacy Policy</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-2 footer-social animated fadeInDown">
+                        <h4>Follow Us</h4>
+                        <ul>
+                            <li><a href="#">Facebook</a></li>
+                            <li><a href="#">Twitter</a></li>
+                            <li><a href="#">Instagram</a></li>
+                            <li><a href="#">RSS</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3 footer-ns animated fadeInRight">
+                        <h4>Newsletter</h4>
+                        <p>A rover wearing a fuzzy suit doesn’t alarm the real penguins</p>
+                        <p>
+                            <div class="input-group">
+                                <input type="text" class="form-control" placeholder="Search for..."></input>
+                                <span class="input-group-btn">
+                                    <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-envelope"></span></button>
+                                </span>
+                            </div>
+                        </p>
+                    </div>
+                </div>
+            </footer>
+        </>
+    );
 }
+
 
 
 
